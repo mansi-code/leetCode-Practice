9e1c7e4c17c606ffc78291aaaa42980722eeb70f
Mansi Jain <56988137+mansi-code@users.noreply.github.com>
2022-02-28 14:38:11 +0530

    Time: 125 ms (55.38%), Space: 59.1 MB (39.45%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        if(height.size()==0)
-            return 0;
-        int startH= 0;
-        int endH= height.size()-1;
-        int ans=0;
-        while(startH<endH){
-            ans= max(ans,(min(height[startH],height[endH]) * (endH-startH)));
-            if(height[startH]<height[endH])
-                startH++;
-            else
-                endH--;
-        }
-        return ans;
+        //the rectangle will be formed when start se leke end tk ke 2 top heights li jaye , 
+        //and unka difference index ka hoga breadth !! 
+        //to loop chalayenge from i to j
+        //jisse jis rectangle ki value highest ho use hum ans m rkeh 
         
+        
+        int i=0;
+        int j=height.size()-1;
+        int ans=0;
+        
+        while(i<j){
+            
+            ans= max(ans, ( min(height[i], height[j])* (j-i))) ;
+            if(height[i]<height[j])
+                i++;
+            else
+                j--;
+        }
+        
+        return ans;
     }
 };
