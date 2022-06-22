84982840d63d0aa3d39760402ac5949feb78509c
Mansi Jain <56988137+mansi-code@users.noreply.github.com>
2022-06-22 16:39:07 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (31.61%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-            vector< vector< int > > result;
-        for(int i=0; i<rowIndex+1; i++){
-           vector<int> row(i +1, 1) ;
+            vector<int> prev(1,1);
+            
+            for(int i=1; i<=rowIndex; i++){
+                vector<int> curr(prev.size()+1, 1);
+                for(int j=1; j<=i; j++){
+                    int diagonal= prev[j-1];
+                    
+                    int above= 0;
+                    if(j<i)
+                        above= prev[j];
+                    curr[j]= diagonal+ above;
+                }
+                prev=curr;
+                
+            }
+            
+        return prev;
+    }
+};
+/*
+for(int i=0; i<=rowIndex; i++){
+           vector<int> curr(prev.size()+1,1) ;
             for(int j=1;j<i; j++){
                 row[j]= result[i-1][j-1]+ result[i-1][j];
             }
             result.push_back(row);
         }
         return result[rowIndex];
-    }
-};
+        */
