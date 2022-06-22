a404f202328f2ff514da08796448936ec51379b2
Mansi Jain <56988137+mansi-code@users.noreply.github.com>
2022-06-22 15:10:07 +0530

    Time: 15 ms (25.30%), Space: 13.6 MB (96.37%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-//     int minCost(vector<int>& cost, int n, int* dp){
-        
-//         if(n==0||n==1)
-//             return 0;
-//         if(dp[n]!=-1)
-//             return dp[n];
-        
-//         int choice1= minCost(cost, n-1,dp)+ cost[n-1];
-//         int choice2= minCost(cost, n-2,dp)+ cost[n-2];
-        
-//         int answer=  min(choice1, choice2);
-//         dp[n]= answer;
-//         return answer;
-//     }
+
+
     
     int minCostClimbingStairs(vector<int>& cost) {
         int n= cost.size();
         
-        int* dp= new int[n+1];
-        dp[0]=0;
-        dp[1]=0;
-        
+        //int* dp= new int[n+1];
+        int choice1=0;
+        int choice2=0;
+        int curr=0;
         for(int i=2; i<=n; i++){
-            dp[i]= min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2]);
+            curr= min(choice1+ cost[i-1], choice2+ cost[i-2]);
+            choice2=choice1;
+            choice1= curr;
+            //dp[i]= min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2]);
         }
-        return dp[n];// cost to reach n
+        return curr;
     }
 };
