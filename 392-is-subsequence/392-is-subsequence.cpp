f477f190ef822202b765ed0bf0ee820d5ba8c57c
Mansi Jain <56988137+mansi-code@users.noreply.github.com>
2022-06-29 12:39:04 +0530

    Time: 7 ms (19.23%), Space: 6.7 MB (20.91%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,19 +1,33 @@
+#include <bits/stdc++.h>
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int i=0;
-        
-        int j=0;
-        while(i<s.size() && j<t.size()){
-            if(t[j]==s[i])
-            {
-                i++;
-            }
-            j++;
+        unordered_map<char, vector<int>> hg;
+        for(int i=0; i<t.size(); i++){
+            hg[t[i]].push_back(i);
         }
-        if(i==s.size())
-            return true;
-        else
-            return false;
+         int prev = -1;
+        for(char c: s){
+            // iterator to find the char 
+            auto it = hg.find(c);
+            //if iterator reached end , return false, i.e doesnt exist
+            
+            if(it == hg.end()) return false;
+            //iterator to make a vector corresponding to char
+            auto vec = it->second;
+            
+            //upperbound returns the value greater than the value in the range
+            //upperbound( start range, end range, value greater than )
+            //subtracticting the begin index to get the exact pos
+            int pos = upper_bound(vec.begin(), vec.end(), prev) - vec.begin(); 
+            
+            //if pos == vector.size meaning didnt find it
+            
+            if(pos == vec.size()) return false;
+
+            //next time prev = vector[pos ] lowest value admitted 
+            prev = vec[pos];
+        }
+        return true;
     }
 };
