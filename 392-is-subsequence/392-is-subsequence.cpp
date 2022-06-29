cb75539f80e07bea0852520391a58fd4d3601c45
Mansi Jain <56988137+mansi-code@users.noreply.github.com>
2022-06-29 12:01:24 +0530

    Time: 4 ms (41.01%), Space: 6.4 MB (71.77%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0;
+        
+        int j=0;
+        while(i<s.size() && j<t.size()){
+            if(t[j]==s[i])
+            {
+                i++;
+            }
+            j++;
+        }
+        if(i==s.size())
+            return true;
+        else
+            return false;
+    }
+};
