9c5c4b7dcb2a38d7bc86f34e4d888e47773e3e83
Mansi Jain <56988137+mansi-code@users.noreply.github.com>
2022-07-11 13:08:55 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (82.91%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -23,14 +23,28 @@ public:
             return 1;
         if(n==2)
             return 1;
-        int *dp= new int[n+1];
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=1;
+        
+        int a= 0;
+        int b= 1;
+        int c=1;
+        int ans=0;
         
         for(int i=3; i<=n; i++){
-            dp[i]=dp[i-1]+ dp[i-2]+ dp[i-3];
+            ans= a+b+c;
+            a=b;
+            b=c;
+            c=ans;
         }
-        return dp[n];
+        
+        return ans;
+//         int *dp= new int[n+1];
+//         dp[0]=0;
+//         dp[1]=1;
+//         dp[2]=1;
+        
+//         for(int i=3; i<=n; i++){
+//             dp[i]=dp[i-1]+ dp[i-2]+ dp[i-3];
+//         }
+//         return dp[n];
     }
 };
