400eb9f419e59056b8e6cbb2cb93cd35d619873a
Mansi Jain <56988137+mansi-code@users.noreply.github.com>
2022-07-22 12:10:29 +0530

    Time: 5 ms (35.29%), Space: 7.4 MB (63.80%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -8,56 +8,20 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-// class Solution {
-// public:
-//     ListNode* reverseBetween(ListNode* head, int left, int right) {
-//         int count=0;
-//         ListNode* prev= NULL;
-//         ListNode* curr= head;
-//         while(count!=left && curr!=NULL){
-//             prev= curr;
-//             curr= curr->next;
-//             count++;
-//         }
-        
-//         ListNode* rightSide = curr;
-//         while(count != right){
-//             rightSide = rightSide->next;
-//             count++;
-//         }
-//         rightSide= rightSide->next;
-        
-//         count=left;
-//         ListNode*  newNode= curr; 
-        
-//         ListNode *nextNode, *prevNode = NULL;
-//         while(newNode && count!=right){
-//             nextNode= newNode->next;
-//             newNode->next= prevNode;
-//             prevNode= newNode;
-//             newNode= nextNode;
-//             count++;
-            
-//         }
-//         //newNode is the head of the reversed linked list
-//         //prev is the tail of the not changed list
-//         if (prev!=NULL){
-//            prev->next = newNode;
-//          }
-//         curr = newNode;
-//         while(curr->next!= NULL){
-//             curr= curr->next;
-//         }
-//         curr->next= rightSide;
-        
-//         return head;
-//     }
-// };
-
 class Solution {
 public:
+//    ListNode *reverse( ListNode* head){
+// ListNode* nextNode, *prevNode= NULL;
+// while(head){
+// 	nextNode= head->next;
+// 	head->next= prevNode;
+// 	prevNode= head;
+// 	head= nextNode;
+// }
+//  return prevNode;
+// }
     
-    ListNode* reverse(ListNode* head){
+      ListNode* reverse(ListNode* head){
         
         ListNode* prev = NULL, *next = NULL, *current = head;
         while(current != NULL){
@@ -71,26 +35,42 @@ public:
         return prev;
     }
     
+    
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        
-        if(head == NULL || left == right){
-            return head;
-        }
-        ListNode* prev, *tail = NULL, *temp = NULL;
-        ListNode dummy(NULL);
-        prev = &dummy;
-        dummy.next = head;
-        for(int i=0; i < left-1; i++){
-            prev = prev->next;
-        }
-        tail = prev->next;
-        for(int i=0; i< right - left;i++){
-            temp = prev->next;
-            prev->next = tail->next;
-            tail->next = tail->next->next;
-            prev->next->next = temp; 
-        }
-        
-        return dummy.next;
-    }
+ListNode* prev= NULL, *curr= head;
+     int count=1;
+	while(count!=left){
+		prev= curr;
+		curr= curr->next;
+		count++;
+	}
+	ListNode* start= curr;
+	while(count!= right){
+		curr= curr->next;
+		count++;
+	}
+	ListNode* rest = curr->next;
+	//prev stores the last value before left to join later
+	//start stores the next value or prev->next
+	//rest stores the rest of the nodes to be joined later 
+	//REVERSE the list from start to curr
+	curr->next=NULL;
+	ListNode *reversed= reverse(start);
+    
+	if(prev)
+		prev->next= reversed;
+	
+	curr= reversed;
+	while(curr->next!=NULL){
+		curr= curr->next;
+	}
+	curr->next= rest;
+	
+	if(left==1)
+		return reversed;
+	else
+		return head;
+	
+}
+
 };
